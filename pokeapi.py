8f8a7a04f0ea80cd1c293a92ca0ee780095023fb
JImMY5232 <98219440+JImMY5232@users.noreply.github.com>
2022-04-24 02:10:10 -0400

    done

--- pokeapi.py
+++ pokeapi.py
@@ -1,9 +1,50 @@
 import requests
 
 
-def get_pokemon_info(url):
+def get_pokemon_info(name):
+
     print("Getting Pokemon info...", end=''),
     
 
-    ulrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" + url
-    resp_msg = requests.get(ulrl)
+    url = "https://pokeapi.co/api/v2/pokemon/" + name 
+    resp_msg = requests.get(url)
+    
+    if resp_msg.status_code == 200:
+        print('success')
+        return resp_msg.json() 
+    else:
+        print('failed. Code:', resp_msg.status_code), 
+        return
+
+def get_pokemon_img_url(name):
+
+    pokemon_dict = get_pokemon_info(name)
+
+    if pokemon_dict:
+        print(pokemon_dict)
+        return pokemon_dict['sprites']['other']['official-artwork']['front_default']
+        
+    else:
+        print("failed image")
+
+def get_poke_list(limit=100, offset=0):
+    url ='https://pokeapi.co/api/v2/pokemon/'
+
+    params = {
+        'limit':limit,
+        'offset': offset
+    }
+
+    resp_img = requests.get(url, params=params)
+
+     
+    if resp_img.status_code == 200:
+        
+        dict = resp_img.json()
+
+        return [p['name'] for p in dict['results']]
+    else:
+        print('Failed to get pokemon list.')
+        print('Response code:',resp_img.status_code)
+        print(resp_img.text) 
+        return
--- pokeimageviewer.py
+++ pokeimageviewer.py
@@ -1,33 +1,83 @@
-from cProfile import label
+
 from tkinter import *
 from tkinter import ttk
 import os
 import sys
 import ctypes
-import types
-from pokeapi import get_pokemon_info
-
+import requests
+from pokeapi import get_poke_list, get_pokemon_img_url  
 
 def main():
     script_dir = sys.path[0]
+    image_dir = os.path.join(script_dir, 'images')
+    if not os.path.isdir(image_dir):
+        os.makedirs(image_dir)
 
     root =Tk()
     root.title("pokemon Image Viewer")
-    app_id = 'COMP593.PokemonImageViewer'
+    app_id = 'COMP.pokeimageviewer'
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
     root.iconbitmap(os.path.join('pokeball_icon.ico'))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    
+
 
     frm = ttk.Frame(root)
-    frm.grid()
+    frm.grid(sticky=(N,S,E,W))
+    frm.columnconfigure(0, weight=1)
+    frm.rowconfigure(0, weight=60)
+    root.geometry('600x600')
+
     
     img_pokemon = PhotoImage(file=os.path.join(script_dir,'pokeball.png'))
     Ibl_image = Label(frm,image=img_pokemon)
-    Ibl_image.grid(row=0, column=0)
-    
+    Ibl_image.grid(row=0, column=0, padx=10, pady=10)
 
+    pokemon_list = get_poke_list(limit=1000)
+    pokemon_list.sort()
+    pokemon_sel_cbo = ttk.Combobox(frm, values=pokemon_list, state='readonly')
+    pokemon_sel_cbo.set('Select a Pokemon')
+    pokemon_sel_cbo.grid(row=1, column=0)
+
+    def handle_pokemon_sel_cbo(event):
+        pokemon_name = pokemon_sel_cbo.get()
+        image_url = get_pokemon_img_url(pokemon_name)
+        image_path =os.path.join(image_dir,pokemon_name +'.png')
+        if down_image_frm_url(image_url, image_path):
+            img_pokemon['file'] = image_path
+
+        pokemon_sel_cbo.bind('<<ComboboxSelected>>', handle_pokemon_sel_cbo)
+    desktop_btn = ttk.Button(frm, text='Set as Desktop Image')
+    desktop_btn.state(['disabled'])
+    desktop_btn.grid(row=2,column=0,padx=10,pady=10)
 
     root.mainloop()
 
+def set_desktop_bg(path):
 
+    try:
+     ctypes.windll.user32.SystemParametersInfoW(20, 0, "absolute path", 0)
+    except:
+        print("Error setting desktop background image")
 
+def down_image_frm_url(url, path):
+
+    if os.path.isfile(path):
+        return path
+
+    resp_msg = requests.get(url)
+    if resp_msg.status_code == 200:
+        try:
+            img_data =resp_msg.content
+            with open(path, 'wb') as fp:
+                fp.write(img_data)
+            return path
+        except:
+            return
+    else:
+        print('Failed to get pokemon list.')
+        print('Response code:',resp_msg.status_code)
+        print(resp_msg.text) 
+        
 main()
