abdd82e5be35dff3d233db280cccd14fe9ace9ff
JImMY5232 <98219440+JImMY5232@users.noreply.github.com>
2022-04-24 03:09:38 -0400

    Update pokeimageviewer.py

--- pokeimageviewer.py
+++ pokeimageviewer.py
@@ -6,13 +6,14 @@ import sys
 import ctypes
 import requests
 from pokeapi import get_poke_list, get_pokemon_img_url  
-
+# create the main fuction
 def main():
+    #create a path 
     script_dir = sys.path[0]
     image_dir = os.path.join(script_dir, 'images')
     if not os.path.isdir(image_dir):
         os.makedirs(image_dir)
-
+#crate root ttk
     root =Tk()
     root.title("pokemon Image Viewer")
     app_id = 'COMP.pokeimageviewer'
@@ -22,24 +23,25 @@ def main():
     root.rowconfigure(0, weight=1)
     
 
-
+#create a frame for gui
     frm = ttk.Frame(root)
     frm.grid(sticky=(N,S,E,W))
     frm.columnconfigure(0, weight=1)
     frm.rowconfigure(0, weight=60)
     root.geometry('600x600')
 
-    
+    #create a image for default window
     img_pokemon = PhotoImage(file=os.path.join(script_dir,'pokeball.png'))
     Ibl_image = Label(frm,image=img_pokemon)
     Ibl_image.grid(row=0, column=0, padx=10, pady=10)
-
+   
+    #creating list of pokemon
     pokemon_list = get_poke_list(limit=1000)
     pokemon_list.sort()
     pokemon_sel_cbo = ttk.Combobox(frm, values=pokemon_list, state='readonly')
     pokemon_sel_cbo.set('Select a Pokemon')
     pokemon_sel_cbo.grid(row=1, column=0)
-
+    #create new fuction for handle event
     def handle_pokemon_sel_cbo(event):
         pokemon_name = pokemon_sel_cbo.get()
         image_url = get_pokemon_img_url(pokemon_name)
@@ -49,7 +51,7 @@ def main():
             desktop_btn.state(['!disabled'])
 
     pokemon_sel_cbo.bind('<<ComboboxSelected>>', handle_pokemon_sel_cbo)
-    
+    #fuction for button
     def desktop_btn_click():
         pokemon_name = pokemon_sel_cbo.get()
         image_path = os.path.join(image_dir, pokemon_name +'.png')
@@ -60,14 +62,14 @@ def main():
     desktop_btn.grid(row=2,column=0,padx=10,pady=10)
 
     root.mainloop()
-
+# create a fuction which make the downloaded picture as system background
 def set_desktop_bg(path):
 
     try:
      ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
     except:
         print("Error setting desktop background image")
-
+#created a fuction  for download the image from the url
 def down_image_frm_url(url, path):
 
     if os.path.isfile(path):
